31155b10f4c744ebb124e9fdef518a2744125b03
LuizRedGalaxy <luizgustavoglt@gmail.com>
2024-05-26 13:15:18 -0300

    Estrutura inicial da interface web

---
+++ web-interface/src/main.ts
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const app = document.getElementById("App");
+
+  app.innerHTML = `
+    <h1>Hello World !</h1>
+  `;
+});
---
+++ web-interface/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = "style-loader";
+
+const config = {
+  entry: "./src/main.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+    open: true,
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
